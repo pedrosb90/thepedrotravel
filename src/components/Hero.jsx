e309700c59f53edecce403faa81aff79dfa9e338
drope <pstrauch@gmail.com>
2026-08-19 14:55:39 -0300

    payouts script/test other

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -75,7 +75,7 @@ const Hero = () => {
       <header className="flex flex-col items-center relative w-full h-[529px] px-7 py-4">
         <div className="flex justify-center items-center">
           <h1 className="font-extrabold text-5xl sm:text-7xl md:text-8xl text-center leading-[55px] sm:leading-[70px] md:leading-[90px] text-gradient">
-            Buscá tus vuelos <br />
+            Insipirá tu <br /> próximo viaje acá
           </h1>
         </div>
 
